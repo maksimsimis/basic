3534e8a97d565a32364d140dd2e5993844936076
farid <farid.lfsibgtu.ru@mail.ru>
2018-10-05 15:27:26 +0700

    modified:   composer.json 	modified:   composer.lock 	modified:   config/web.php

--- config/web.php
+++ config/web.php
@@ -3,19 +3,11 @@
 $config = [
     'id' => 'basic',
     'basePath' => dirname(__DIR__),
-    'aliases' => [
-        '@bower' => '@vendor/bower-asset',
-    ],
     'components' => [
         'request' => [
             // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
             'cookieValidationKey' => 'U4gw2GU269ZSEB3id-OoGuz5yf76PgCY',
         ],
-        'user' => [
-            'identityClass' => 'app\models\User',
-            'enableAutoLogin' => true,
-        ],
-
     ],
 ];
 
