03728e998c2a642cef6a7e35e8d1a6244d6462b1
farid <farid.lfsibgtu.ru@mail.ru>
2018-10-15 16:24:55 +0700

    modified:   controllers/SiteController.php

--- controllers/SiteController.php
+++ controllers/SiteController.php
@@ -10,4 +10,18 @@ class SiteController extends Controller {
         return "Hello World!";
     }
 
+    public function actionEntry() {
+        $model = new EntryForm();
+
+        if ($model->load(Yii::$app->request->post()) && $model->validate()) {
+            // данные в $model удачно проверены
+            // делаем что-то полезное с $model ...
+
+            return $this->render('entry-confirm', ['model' => $model]);
+        } else {
+            // либо страница отображается первый раз, либо есть ошибка в данных
+            return $this->render('entry', ['model' => $model]);
+        }
+    }
+
 }
