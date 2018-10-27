1d0d0b6318d6a5dd961fbdc4b9645099158ce6e7
farid <farid.lfsibgtu.ru@mail.ru>
2018-10-27 14:51:25 +0700

    yii2-httpclient

--- controllers/SiteController.php
+++ controllers/SiteController.php
@@ -5,6 +5,9 @@ namespace app\controllers;
 use yii\web\Controller;
 use app\models\EntryForm;
 use Yii;
+//use GuzzleHttp\Client; // подключаем Guzzle
+use yii\helpers\Url;
+use yii\httpclient\Client;
 
 class SiteController extends Controller {
 
@@ -19,11 +22,34 @@ class SiteController extends Controller {
             // данные в $model удачно проверены
             // делаем что-то полезное с $model ...
             $model->save();
+            $model::updateAll(['email' => '0'], ['name' => 'name']);
             return $this->render('entry-confirm', ['model' => $model]);
         } else {
             // либо страница отображается первый раз, либо есть ошибка в данных
             return $this->render('entry', ['model' => $model]);
         }
+    }
+
+    public function actionMeteo() {
+
+        // создаем экземпляр класса
+        $client = new Client();
+        // отправляем запрос к странице Яндекса
+        //$res = $client->request('GET', 'https://ya.ru');
+        $res = $client->createRequest()
+                ->setMethod('post')
+                ->setUrl('https://www.gismeteo.ru/diary/4638/2018/10/')
+                ->setData(['Year' => '2018',
+                    'sd_country' => 'Россия',
+                    'sd_distr' => 'Красноярский край',
+                    'sd_city' => 'Мотыгино',
+                    'Month' => 'Октябрь'])
+                ->send();
+        // получаем данные между открывающим и закрывающим тегами body
+        //$body = $res->getBody();
+        $body = $res->content;
+        // вывод страницы Яндекса в представление
+        return $this->render('meteo', ['body' => $body]);
     }
 
 }
